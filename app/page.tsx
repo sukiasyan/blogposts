ffb3823d49d783999956c5c95ea784364d4fb4f9
hsukiasyan <hakob.sukiasyan@pricespider.com>
2023-05-20 07:32:11 +0200

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome &nbsp;
+        <span className="whitespace-nowrap">
+          I`m <span className="font-bold"> Hakob</span>
+        </span>
+      </p>
+    </main>
+  )
+}
